77348957d13af84ce8ab3a1af36c08b4b2aff8c4
El-Farooq <farooqahmadbbd@gmail.com>
2025-03-21 22:29:35 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const courseRouter = require('./src/routes/courseRoutes');
+const assignmentRouter = require('./src/routes/assignmentRoutes');
 
 const app = express()
 const db = require('./src/config/db')
@@ -10,9 +12,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const appRouter = require('./src/app');
-const courseRouter = require('./src/routes/courseRoutes');
-const { assign } = require('nodemailer/lib/shared');
-const assignmentRouter = require('./src/routes/assignmentRoutes');
+//const { assign } = require('nodemailer/lib/shared');
 app.use("/api", appRouter)
 app.use("/api/courses", courseRouter);
 app.use("/api/assignments", assignmentRouter);
