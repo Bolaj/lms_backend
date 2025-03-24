4de45b6d20b91364ff408528f6d11e7f8218095b
TimiBolaji <mattybolaji@gmail.com>
2025-03-24 22:22:30 +0100

    fix:--

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const courseRouter = require('./src/routes/courseRoutes');
-const assignmentRouter = require('./src/routes/assignmentRoutes');
-
 const app = express()
 const db = require('./src/config/db');
 db()
@@ -12,10 +9,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const appRouter = require('./src/app');
-//const { assign } = require('nodemailer/lib/shared');
 app.use("/api", appRouter)
-app.use("/api/courses", courseRouter);
-app.use("/api/assignments", assignmentRouter);
+
 
 
 const port = process.env.PORT
