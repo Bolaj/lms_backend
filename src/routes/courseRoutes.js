44a9cd36352ab88f82d6fe470e7842470f1b3350
TimiBolaji <mattybolaji@gmail.com>
2025-03-25 23:16:52 +0100

    fix: update course service

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const {verifyUser} = require('../auth/verifyAdminMiddleware');
-const { createCourse, getCourses, enrollStudent } = require("../controllers/courseController");
+const {verifyUser, verifyToken} = require('../auth/verifyAdminMiddleware');
+const { createCourse, getCourses, enrollStudent, updateCourse, softDeleteCourse, deleteCoursePermanently, restoreCourse } = require("../controllers/courseController");
 
 const courseRouter = express.Router();
 
 courseRouter.post("/", verifyUser,  createCourse);
 courseRouter.get("/", verifyUser, getCourses);
-courseRouter.post("/:courseId/enroll", enrollStudent);
+courseRouter.post("/:courseCode/enroll", verifyToken, enrollStudent);
+courseRouter.put("/:courseCode", verifyUser, updateCourse);
+courseRouter.post("/:courseCode/soft-delete", verifyUser, softDeleteCourse);
+courseRouter.delete("/:courseCode/delete", verifyUser, deleteCoursePermanently);
+courseRouter.post("/:courseCode/restore", verifyUser, restoreCourse);
+
 
 module.exports = courseRouter;
