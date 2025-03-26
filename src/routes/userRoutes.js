6350f891b7a1e0a1a066d5ab283bfdb0037fbb63
TimiBolaji <mattybolaji@gmail.com>
2025-03-26 02:28:55 +0100

    fix: update user route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const { validateSignup } = require('../utils/validator')
-const {signUp, verifyAccount, resendVerificationCode, signIn, enrollStudent } = require ('../controllers/userController')
+const {signUp, verifyAccount, resendVerificationCode, signIn } = require ('../controllers/userController')
 const userRouter = express.Router()
 const {  updateUser } = require("../controllers/userController");
-const { verifyUser } = require("../auth/verifyAdminMiddleware"); 
+const { verifyToken } = require("../auth/verifyAdminMiddleware"); 
 
 userRouter.post('/user/signup', validateSignup, signUp)
 userRouter.post('/user/verify-account', verifyAccount)
 userRouter.post('/user/resend-verification-code', resendVerificationCode)
-userRouter.put('/user/update/:id', verifyUser,  updateUser);
+userRouter.put('/user/update/:id', verifyToken,  updateUser);
 userRouter.post('/user/signIn', signIn)
 
 
