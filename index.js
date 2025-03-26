0059fd0376a681c108e4b47f96c79df7c70a2be5
TimiBolaji <mattybolaji@gmail.com>
2025-03-26 02:30:07 +0100

    feat: api documentation in progress

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocs = YAML.load('./src/swagger/swagger.yml')
+
 const app = express()
 const db = require('./src/config/db');
 db()
@@ -10,13 +14,15 @@ dotenv.config()
 
 const appRouter = require('./src/app');
 app.use("/api", appRouter)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 
 
 const port = process.env.PORT
-
-app.listen(port, () => console.log("App listening on port", port))
-
+app.listen(port, () => {
+  console.log("App listening on port", port);
+  console.log("Swagger UI available at http://localhost:4000/api-docs");
+});
 app.get('/', (req, res) => {
   return res.status(200).json({
     message: "Welcome to the School Learning Management System (LMS) is a platform designed to help schools efficiently manage students, teachers, courses, assignments, and communication in an online environment. The system provides role-based access control (RBAC) for administrators, teachers, and students."
